438b4606a03b9c08f77de5ff0f97151a32a25edb
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2023-10-30 23:38:34 +0900

    Globalの履歴ファイルを追加

--- history.zsh
+++ history.zsh
@@ -1,9 +1,10 @@
 ZSH_COMMAND_HISTORY_TOOLDIR=${ZSH_COMMAND_HISTORY_TOOLDIR-${0:A:h}}
 HISTORY_BASE_DIR="${XDG_DATA_HOME-$HOME/.local/share}/zsh/CommandHistory/"
+HISTORY_GLOBAL_FILE="${HISTORY_BASE_DIR}/.__history"
 
 function fzf-command-history() {
     local history_local_file
-    history_local_file="${HISTORY_BASE_DIR}/${PWD}/.__history"
+    history_local_file="${HISTORY_BASE_DIR}/local/${PWD}/.__history"
     read _CURSOR _BUFFER <<< $("${ZSH_COMMAND_HISTORY_TOOLDIR}/rust/command_history/target/release/command_history"  "${history_local_file}")
     if [[ -n "$_CURSOR" ]] && [[ -n "$_BUFFER" ]]; then
         BUFFER="${_BUFFER} "
@@ -16,7 +17,11 @@ bindkey "^t" fzf-command-history
 
 function preexec() {
     local history_local_file
-    history_local_file="${HISTORY_BASE_DIR}/${PWD}/.__history"
+    history_local_file="${HISTORY_BASE_DIR}/local/${PWD}/.__history"
     mkdir -p $(dirname ${history_local_file})
-    echo "$1" | sed "s/^/$(date '+%Y-%m-%dT%H:%M:%S%z')\t/" >> ${history_local_file}
+    echo "$1" | \
+        sed "s/^/$(date '+%Y-%m-%dT%H:%M:%S%z')\t/" | \
+        tee -a ${history_local_file} | \
+        paste - <(echo "\"${PWD}\"") \
+        >> "${HISTORY_GLOBAL_FILE}"
 }
